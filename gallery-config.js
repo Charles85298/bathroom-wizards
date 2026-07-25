e4c4ba84d7468f9eb82de47f74cb52a6c60341c1
Charles85298 <charles.g.fleming@gmail.com>
2026-07-25 13:32:12 -0700

    delet4e

--- gallery-config.js
+++
@@ -1,8 +0,0 @@
-// Bathroom Wizards Version 3 gallery configuration
-// Edit only the values below once, then commit this file to GitHub.
-window.BATHROOM_WIZARDS_GALLERY = {
-  owner: "Charles85298",
-  repo: "bathroom-wizards",
-  branch: "main",
-  folder: "assets/gallery"
-};
